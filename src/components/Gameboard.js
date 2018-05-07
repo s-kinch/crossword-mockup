5bca495e0b50847a0fea6a1a00cf28b38effe9ce
sinclairstudy <sinclairstudy@gmail.com>
2018-05-07 09:51:02 -0400

    we have a grid

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -4,28 +4,53 @@ class Gameboard extends React.Component {
 
   render(){
     const grid = []
-    for (let i = 0; i < 225; i++){
+    for (let i = 0; i < 15; i++){
+      const row = []
+      for (let i = 0; i < 15; i++){
+        row.push(
+          <td>
+            <div className="ui card tablesquare" >
+              <div className="content">
+                { i }
+              </div>
+            </div>
+          </td>
+        )
+      }
       grid.push(
-
-      <div className="column square" >
-        <div className="ui card" >
-          <div className="content">
-            { i }
-          </div>
-        </div>
-      </div>
+        <tr>
+          { row }
+        </tr>
       )
     }
+    //   <div className="column square" >
+    //     <div className="ui card" >
+    //       <div className="content">
+    //         { i }
+    //       </div>
+    //     </div>
+    //   </div>
+    //   )
+    // }
+
+
 
 
     return(
-      <div className="ui page grid gameboard" >
-        <div className="fifteen column row">
-          <img className="board" src="/board.png"/>
-          {grid}
+      // <div className="ui page grid gameboard" >
+      //   <div className="fifteen column row">
+      //     <img className="board" src="/board.png"/>
+      //     {grid}
+      //   </div>
+      // </div>
+      <div className="tableboard">
+        <img className="board" src="/board.png"/>
+        <div className="table">
+          <table>
+            { grid }
+          </table>
         </div>
       </div>
-
     )
   }
 
